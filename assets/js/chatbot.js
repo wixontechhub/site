78d6c93b4bc8af2a0ba1ac1ccab8f06dc1382998
wixontechhub <wixontechhub@gmail.com>
2024-08-12 18:21:20 +0530

    Add files via upload

---
+++ assets/js/chatbot.js
@@ -0,0 +1,15 @@
+
+window.embeddedChatbotConfig = {
+    chatbotId: "-XqYWmcGH0Co3Q6U2atfr",
+    domain: "www.chatbase.co"
+};
+
+// Additional chatbot script
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://www.chatbase.co/embed.min.js";
+    script.setAttribute('chatbotId', "-XqYWmcGH0Co3Q6U2atfr");
+    script.setAttribute('domain', "www.chatbase.co");
+    script.defer = true;
+    document.head.appendChild(script);
+})();
